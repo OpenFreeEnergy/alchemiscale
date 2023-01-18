c420c60c1f5dae84c4d0e36164cd8d4338a21854
Hugo MacDermott <hugomacdermott@gmail.com>
2023-01-19 10:10:45 +1100

    change theme and fix up

--- docs/conf.py
+++ docs/conf.py
@@ -12,13 +12,17 @@ author = '"OpenFE and OpenFF developers"'
 
 import sys
 import os
-import alabaster
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.githubpages',
+              'sphinx_sitemap',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -28,7 +32,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [
+    sphinx_rtd_theme.get_html_theme_path()
+]
 html_static_path = ['_static']
 
 
