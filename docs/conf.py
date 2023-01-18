e514b9a3b5b855ea1e4913ba08eab4864e1f2dab
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2023-01-19 10:27:32 +1100

    Attempt to  fix GH-pages docs (#67)
    * explicitly import alabaster
    
    * change theme and fix up
    
    * remove sitempa
    
    * add nojekyll

--- docs/conf.py
+++ docs/conf.py
@@ -6,26 +6,32 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'fah-alchemy'
+project = "fah-alchemy"
 copyright = '2022, "OpenFE and OpenFF developers"'
 author = '"OpenFE and OpenFF developers"'
 
 import sys
 import os
-sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
+
+sys.path.insert(0, os.path.abspath("."))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ["_static"]
