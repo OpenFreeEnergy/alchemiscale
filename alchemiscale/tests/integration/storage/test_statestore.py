9e61af39e85a68c7eb4e3256ce40bcc4074aff74
David Dotson <dotsdl@gmail.com>
2023-04-14 11:37:30 -0700

    Make hearteat test more reliable

--- alchemiscale/tests/integration/storage/test_statestore.py
+++ alchemiscale/tests/integration/storage/test_statestore.py
@@ -335,8 +335,11 @@ class TestNeo4jStore(TestStateStore):
             """
         ).to_subgraph()
 
-        assert csreg["registered"] == now
-        assert csreg["heartbeat"] == tomorrow
+        # we round to integer seconds from epoch to avoid somewhat different
+        # floats on either side of comparison even if practically the same
+        # straight datetime comparisons would sometimes fail depending on timing
+        assert int(csreg["registered"].to_native().timestamp()) == int(now.timestamp())
+        assert int(csreg["heartbeat"].to_native().timestamp()) == int(tomorrow.timestamp())
 
     def test_expire_registrations(self, n4js, compute_service_id):
         now = datetime.utcnow()
