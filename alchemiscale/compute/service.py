ee8b8a100934344488531c8c084f9d8feeedbf36
David Dotson <dotsdl@gmail.com>
2023-04-17 11:36:15 -0700

    Added logging statements around ProtocolDAG creation

--- alchemiscale/compute/service.py
+++ alchemiscale/compute/service.py
@@ -304,7 +304,9 @@ class SynchronousComputeService:
 
         """
         # obtain a ProtocolDAG from the task
+        self.logger.info("Creating ProtocolDAG from task '%s'...", task)
         protocoldag, transformation, extends = self.task_to_protocoldag(task)
+        self.logger.info("Created ProtocolDAG '%s' from task '%s'", protocoldag, task)
 
         # execute the task; this looks the same whether the ProtocolDAG is a
         # success or failure
