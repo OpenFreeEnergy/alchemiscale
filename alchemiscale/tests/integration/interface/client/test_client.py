3018bb3d7e484fc19ebea330eec686b50db5bf82
Ian Kenney <ianmichaelkenney@gmail.com>
2023-12-29 09:07:56 -0700

    don't pop the transformation components

--- alchemiscale/tests/integration/interface/client/test_client.py
+++ alchemiscale/tests/integration/interface/client/test_client.py
@@ -1157,9 +1157,9 @@ class TestClient:
         # needed to create the transformation. Therefore we need to clear the registry
         # of these objects to be sure that the correct objects are returned from the
         # database.
-        with RegistryBackup(gufe_object=transformation):
-            # user client : pull transformation results, evaluate
-            protocolresult = user_client.get_transformation_results(transformation_sk)
+        # with RegistryBackup(gufe_object=transformation):
+        # user client : pull transformation results, evaluate
+        # protocolresult = user_client.get_transformation_results(transformation_sk)
 
         assert protocolresult.get_estimate() == 95500.0
         assert set(protocolresult.data.keys()) == {"logs", "key_results"}
