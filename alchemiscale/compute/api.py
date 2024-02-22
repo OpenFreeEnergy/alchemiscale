06f44ec710ea3d74cbc002f24f0f5c100fcda71d
David L. Dotson <dotsdl@gmail.com>
2024-02-21 23:27:09 -0700

    Update alchemiscale/compute/api.py
    Remove unused imports.
    
    Co-authored-by: Ian Kenney <ianmichaelkenney@gmail.com>

--- alchemiscale/compute/api.py
+++ alchemiscale/compute/api.py
@@ -9,8 +9,7 @@ import os
 import json
 from datetime import datetime, timedelta
 
-from fastapi import FastAPI, APIRouter, Body, Depends, HTTPException
-from fastapi import status as http_status
+from fastapi import FastAPI, APIRouter, Body, Depends
 from fastapi.middleware.gzip import GZipMiddleware
 from gufe.tokenization import GufeTokenizable, JSON_HANDLER
 
