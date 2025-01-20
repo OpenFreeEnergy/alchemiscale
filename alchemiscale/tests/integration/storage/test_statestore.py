e6d1ece14e7a856674f3534bd8a5cd565e2aff78
David Dotson <dotsdl@gmail.com>
2025-01-20 13:49:01 -0700

    Test fix

--- alchemiscale/tests/integration/storage/test_statestore.py
+++ alchemiscale/tests/integration/storage/test_statestore.py
@@ -2148,7 +2148,7 @@ class TestNeo4jStore(TestStateStore):
             if status == "complete":
                 n4js.set_task_running(task_scoped_keys)
 
-            assert n4js.set_task_status(task_scoped_keys)[0] is not None
+            assert n4js.set_task_status(task_scoped_keys, status)[0] is not None
 
             query = """
             MATCH (:TaskRestartPattern)-[:APPLIES]->(task:Task)
