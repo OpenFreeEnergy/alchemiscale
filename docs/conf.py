42879e41a0d854e779d52f2f7f0f00e7fa167183
Ian Kenney <ianmichaelkenney@gmail.com>
2025-03-10 12:25:28 -0700

    Use latest Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -63,8 +63,6 @@ intersphinx_mapping = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
 
 # -- Options for MystNB ------------------------------------------------------
 
