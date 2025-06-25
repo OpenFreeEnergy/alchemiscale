20bef5326e1dd84a7f5c6f4108746cf91d6bc953
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-06-25 15:48:47 -0700

    typo

--- alchemiscale/compute/api.py
+++ alchemiscale/compute/api.py
@@ -103,7 +103,7 @@ def register_computeservice(
     compute_service_id,
     n4js: Neo4jStore = Depends(get_n4js_depends),
 ):
-    datetime.datetime.now(tz=datetime.UTC)
+    now = datetime.datetime.now(tz=datetime.UTC)
     csreg = ComputeServiceRegistration(
         identifier=ComputeServiceID(compute_service_id),
         registered=now,
@@ -134,7 +134,7 @@ def heartbeat_computeservice(
     n4js: Neo4jStore = Depends(get_n4js_depends),
     settings: ComputeAPISettings = Depends(get_base_api_settings),
 ):
-    datetime.datetime.now(tz=datetime.UTC)
+    now = datetime.datetime.now(tz=datetime.UTC)
 
     # expire any stale registrations, along with their claims
     expire_delta = timedelta(
@@ -384,7 +384,7 @@ async def set_task_result(
         n4js.set_task_error(tasks=[task_sk])
 
         # report that the compute service experienced a failure
-        datetime.datetime.now(tz=datetime.UTC)
+        now = datetime.datetime.now(tz=datetime.UTC)
         n4js.log_failure_compute_service(compute_service_id, now)
         n4js.resolve_task_restarts(task_scoped_keys=[task_sk])
 
